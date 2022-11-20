f1a8bfc9ca642e62f06f9caa1ab2b1d571eb00ff
Blackflame6 <engineerfranklyn@gmail.com>
2022-11-19 20:32:37 -0600

    finished tours

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const newTours = (id) => {
+    const removeTours = tours.filter((oneTour) => {
+      return oneTour.id !== id;
+    });
+    setTours(removeTours);
+  };
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTours(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length == 0) {
+    return (
+      <main>
+        <h3 className="title">No Tours</h3>
+        <button className="btn" onClick={fetchTours}>
+          {" "}
+          Refresh
+        </button>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tours} newTours={newTours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- client/src/Tours.js
+++ client/src/Tours.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, newTours }) => {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour)=> (
+          <Tour key={tour.id} {...tour} newTours={newTours}/>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
